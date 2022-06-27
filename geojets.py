803d12d2ce4855db7797e44693d220a80bc540a4
lmh00 <lmh004@protonmail.com>
2022-06-27 15:27:20 +0900

    removed the background movement, need to create coordinates system instead

--- geojets.py
+++ geojets.py
@@ -15,63 +15,50 @@ class Cursor(py.sprite.Sprite):
 class Player(py.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.jet = py.image.load('images/jet.png').convert_alpha()
-        self.image = self.jet
+        self.img = py.image.load('images/jet.png').convert_alpha()
+        self.image = self.img
         self.rect = self.image.get_rect(center = (HEIGHT / 2, WIDTH / 2))
         self.speed = 3
 
     def move(self):
-        pressed = py.key.get_pressed()
-        if pressed[py.K_a] or pressed[py.K_LEFT]:
-            bg.sprite.rect.centerx += self.speed
-        if pressed[py.K_d] or pressed[py.K_RIGHT]:
-            bg.sprite.rect.centerx -= self.speed
-        if pressed[py.K_w] or pressed[py.K_UP]:
-            bg.sprite.rect.centery += self.speed
-        if pressed[py.K_s] or pressed[py.K_DOWN]:
-            bg.sprite.rect.centery -= self.speed
+        # pressed = py.key.get_pressed()
+        # if pressed[py.K_a] or pressed[py.K_LEFT]:
+        #      += self.speed
+        # if pressed[py.K_d] or pressed[py.K_RIGHT]:
+        #      -= self.speed
+        # if pressed[py.K_w] or pressed[py.K_UP]:
+        #      += self.speed
+        # if pressed[py.K_s] or pressed[py.K_DOWN]:
+        #      -= self.speed
+        pass
 
     def rotate(self):
         mX, mY = py.mouse.get_pos()
         angleRad = math.atan2(self.rect.centery - mY, mX - self.rect.centerx)
         angleDeg = math.degrees(angleRad) - 90
-        self.image = py.transform.rotate(self.jet, angleDeg)
+        self.image = py.transform.rotate(self.img, angleDeg)
         self.rect = self.image.get_rect(center = (self.rect.centerx, self.rect.centery))
 
     def update(self):
         self.move()
         self.rotate()
 
-class Background(py.sprite.Sprite):
+class Camera(py.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = py.image.load('images/1.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft = (0, 0))
-
-    def render(self):
-        if bg.sprite.rect.top > 0 and bg.sprite.rect.top < 251:
-            self.image = py.image.load('images/1.png').convert_alpha()
-        if bg.sprite.rect.top > 250 and bg.sprite.rect.top < 501:
-            self.image = py.image.load('images/2.png').convert_alpha()
-        if bg.sprite.rect.top > 500 and bg.sprite.rect.top < 751:
-            self.image = py.image.load('images/3.png').convert_alpha()
-        if bg.sprite.rect.top > 750 and bg.sprite.rect.top < 1001:
-            self.image = py.image.load('images/4.png').convert_alpha()
-
-    def update(self):
-        self.render()
+        self.image = py.image.load(self.file).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [self.pos_x, self.pos_y]
 
 py.init()
 screen = py.display.set_mode((HEIGHT, WIDTH))
-geojets_icon = py.image.load('./images/globe.png').convert_alpha()
+geojets_icon = py.image.load('images/globe.png').convert_alpha()
 py.display.set_icon(geojets_icon)
 py.display.set_caption('Geo Jets!')
 py.mouse.set_visible(False)
 clock = py.time.Clock()
 game_active = True
 
-bg = py.sprite.GroupSingle()
-bg.add(Background())
 player = py.sprite.GroupSingle()
 player.add(Player())
 cursor = py.sprite.GroupSingle()
@@ -84,8 +71,6 @@ while game_active:
             exit()
 
     screen.fill(BGCOLOR)
-    bg.draw(screen)
-    bg.update()
     player.draw(screen)
     player.update()
     cursor.draw(screen)
