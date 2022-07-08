7a0af2cbc56f054522b14930fe2a3b45b72f3ee7
lmh00 <lmh004@protonmail.com>
2022-07-08 12:11:13 +0900

    .

--- geojets.py
+++ geojets.py
@@ -28,14 +28,15 @@ class FPS_Counter():                                                    #####
 class Player(py.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.img = jet_icon
+        self.img = square_icon
         self.image = self.img
         self.rect = self.image.get_rect(center = (WIDTH / 2, HEIGHT / 2))
         self.x = 5000
         self.y = 5000
-        self.speed = 10
+        self.angle = 0
+        self.speed = 3
 
-    def move(self):
+    def turn(self):
         pressed = py.key.get_pressed()
         if pressed[py.K_a] or pressed[py.K_LEFT]:
             self.x -= self.speed
@@ -53,9 +54,31 @@ class Player(py.sprite.Sprite):
         self.image = py.transform.rotate(self.img, angleDeg)
         self.rect = self.image.get_rect(center = (self.rect.centerx, self.rect.centery))
 
+    def load(self):
+        click = py.mouse.get_pressed(3)[0]
+        if click:
+            new_bullet = bullet.add(Bullet(self.x, self.y))
+            bullets.append(new_bullet)
+
+    def update(self):
+        self.turn()
+        self.rotate()
+        self.load()
+
+class Bullet(py.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.img = bullet_icon
+        self.image = self.img
+        self.rect = self.image.get_rect(center = (WIDTH / 2, HEIGHT / 2))
+        self.position = [x, y]
+        self.speed = 1
+
+    def move(self):
+        self.position[1] += self.speed
+
     def update(self):
         self.move()
-        self.rotate()
 
 # Settings
 WIDTH = 1200
@@ -69,15 +92,16 @@ screen = py.display.set_mode((WIDTH, HEIGHT), py.HWSURFACE | py.DOUBLEBUF | py.S
 font = py.font.SysFont("Arial" , 18 , bold = True)
 clock = py.time.Clock()
 py.mouse.set_visible(False)
-py.display.set_caption('Geo Jets!')
+py.display.set_caption('Geo Gats!')
 game_active = True
 
 # Images
 map = py.image.load('images/bg/eq_earth.png').convert_alpha()
 cursor_icon = py.image.load('images/cursor.png').convert_alpha()
-jet_icon = py.image.load('images/jet.png').convert_alpha()
+square_icon = py.image.load('images/square.png').convert_alpha()
 logo_icon = py.image.load('images/globe.png').convert_alpha()
 py.display.set_icon(logo_icon)
+bullet_icon = py.image.load('images/bullet.png').convert_alpha()
 
 #Groups
 background = Background()
@@ -85,7 +109,11 @@ player = py.sprite.GroupSingle()
 player.add(Player())
 cursor = py.sprite.GroupSingle()
 cursor.add(Cursor())
+bullet = py.sprite.GroupSingle()
 fps_counter = FPS_Counter()
+
+# Global Variables
+bullets = []
 
 # Game loop
 while game_active:
