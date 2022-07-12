9236a5c4fff25c3bd501250cedb48d0bd762df1e
lmh00 <lmh004@protonmail.com>
2022-07-12 20:01:14 +0900

    https://www.youtube.com/watch?v=3DeW-7vbc50

--- geojets.py
+++ geojets.py
@@ -19,22 +19,20 @@ class Cursor(py.sprite.Sprite):
     def update(self):
         self.move()
 
-class FPS_Counter():                                                    #####
-    def update(self):                                                   #####
-        fps = str(int(clock.get_fps()))                                 #####
-        fps_t = font.render(fps , 1, py.Color("RED"))                   #####
-        screen.blit(fps_t,(0,0))                                        #####
+class FPS_Counter():
+    def update(self):
+        fps = str(int(clock.get_fps()))
+        fps_t = font.render(fps , 1, py.Color("RED"))
+        screen.blit(fps_t,(0,0))
 
 class Player(py.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.img = square_icon
+        self.img = jet_icon
         self.image = self.img
         self.rect = self.image.get_rect(center = (WIDTH / 2, HEIGHT / 2))
         self.x = 5000
         self.y = 5000
-        self.angle = 0
-        self.speed = 3
 
     def turn(self):
         pressed = py.key.get_pressed()
@@ -48,37 +46,38 @@ class Player(py.sprite.Sprite):
             self.y += self.speed
 
     def rotate(self):
-        mX, mY = py.mouse.get_pos()
-        angleRad = math.atan2(self.rect.centery - mY, mX - self.rect.centerx)
+        mx, my = py.mouse.get_pos()
+        angleRad = math.atan2(self.rect.centery - my, mx - self.rect.centerx)
         angleDeg = math.degrees(angleRad) - 90
         self.image = py.transform.rotate(self.img, angleDeg)
         self.rect = self.image.get_rect(center = (self.rect.centerx, self.rect.centery))
 
-    def load(self):
-        click = py.mouse.get_pressed(3)[0]
+    def shoot(self):
+        click = py.mouse.get_pressed() == (1, 0, 0)
+        mouse = py.mouse.get_pos()
         if click:
-            new_bullet = bullet.add(Bullet(self.x, self.y))
-            bullets.append(new_bullet)
+            b = Bullet(self.x, self.y, mouse[0], mouse[1])
+            bullets.append(b)
 
     def update(self):
         self.turn()
         self.rotate()
-        self.load()
+        self.shoot()
 
 class Bullet(py.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, px, py, mx, my):
         super().__init__()
         self.img = bullet_icon
         self.image = self.img
         self.rect = self.image.get_rect(center = (WIDTH / 2, HEIGHT / 2))
-        self.position = [x, y]
         self.speed = 1
+        angle = math.atan2(my - px, mx - py)
+        self.dx = math.cos(angle) * self.speed
+        self.dy = math.sin(angle) * self.speed
 
     def move(self):
-        self.position[1] += self.speed
-
-    def update(self):
-        self.move()
+        self.rect.x = self.rect.x + int(self.dx)
+        self.rect.y = self.rect.y + int(self.dy)
 
 # Settings
 WIDTH = 1200
@@ -92,16 +91,19 @@ screen = py.display.set_mode((WIDTH, HEIGHT), py.HWSURFACE | py.DOUBLEBUF | py.S
 font = py.font.SysFont("Arial" , 18 , bold = True)
 clock = py.time.Clock()
 py.mouse.set_visible(False)
-py.display.set_caption('Geo Gats!')
+py.display.set_caption('Geo Jets!')
 game_active = True
 
-# Images
+# Images -- you can put this all in one image
 map = py.image.load('images/bg/eq_earth.png').convert_alpha()
-cursor_icon = py.image.load('images/cursor.png').convert_alpha()
-square_icon = py.image.load('images/square.png').convert_alpha()
-logo_icon = py.image.load('images/globe.png').convert_alpha()
-py.display.set_icon(logo_icon)
 bullet_icon = py.image.load('images/bullet.png').convert_alpha()
+cursor_icon = py.image.load('images/cursor.png').convert_alpha()
+jet_icon = py.image.load('images/jet.png').convert_alpha()
+logo_icon = py.image.load('images/globe.png').convert_alpha()
+py.display.set_icon(logo_icon) # has to come after logo_icon
+
+#Global Variables
+bullets = []
 
 #Groups
 background = Background()
@@ -111,9 +113,6 @@ cursor = py.sprite.GroupSingle()
 cursor.add(Cursor())
 bullet = py.sprite.GroupSingle()
 fps_counter = FPS_Counter()
-
-# Global Variables
-bullets = []
 
 # Game loop
 while game_active:
@@ -128,7 +127,10 @@ while game_active:
     player.update()
     cursor.draw(screen)
     cursor.update()
-    fps_counter.update()                                                       #####
+    fps_counter.update()
+
+    for b in bullets:
+        b.move()
 
     py.display.update()
     py.event.pump()
